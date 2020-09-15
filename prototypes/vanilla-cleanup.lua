286a83e44f536f649a98f6b0bd643000f5b6d96b
Niet <niet@pokefarm.com>
2020-09-15 09:21:00 +0100

    Pipes on water also blocked player on conveyor

--- prototypes/vanilla-cleanup.lua
+++ prototypes/vanilla-cleanup.lua
@@ -1,6 +1,6 @@
 require("prototypes.vanilla-cleanup.disable-vanilla-recipes")
 require("prototypes.vanilla-cleanup.hide-vanilla-techs")
-require("prototypes.vanilla-cleanup.fix-water-tiles")
+require("prototypes.vanilla-cleanup.allow-pipes-on-water")
 
 -- remove fishing
 data.raw.fish.fish.minable = nil
