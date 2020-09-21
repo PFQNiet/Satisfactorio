4888da3e399c5dba8b2439ae7326d898c8868e2b
Niet <niet@pokefarm.com>
2020-09-21 08:14:45 +0100

    refactor space elevator upgrades

---
+++ scripts/lualib/space-elevator-upgrades.lua
@@ -0,0 +1,11 @@
+return {
+	["space-elevator-phase1"] = {
+		"hub-tier3-coal-power",
+		"hub-tier3-vehicular-transport",
+		"hub-tier3-basic-steel-production",
+		"hub-tier4-advanced-steel-production",
+		"hub-tier4-improved-melee-combat",
+		-- "hub-tier4-hyper-tubes",
+		"hub-tier4-logistics-mk-3"
+	}
+}
--- scripts/special/space-elevator.lua
+++ scripts/special/space-elevator.lua
@@ -96,17 +96,7 @@ local function launchFreighter(hub, item)
 	silo.rocket_parts = 1
 end
 
-local upgrades = {
-	["space-elevator-phase1"] = {
-		"hub-tier3-coal-power",
-		"hub-tier3-vehicular-transport",
-		"hub-tier3-basic-steel-production",
-		"hub-tier4-advanced-steel-production",
-		"hub-tier4-improved-melee-combat",
-		-- "hub-tier4-hyper-tubes",
-		"hub-tier4-logistics-mk-3"
-	}
-}
+local upgrades = require("scripts.lualib.space-elevator-upgrades")
 local function completeElevator(technology)
 	if string.starts_with(technology.name, "space-elevator") then
 		if not upgrades[technology.name] then
