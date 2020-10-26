c13e7d25cedd22e87ac3a9bd5118020239c37079
Niet <niet@pokefarm.com>
2020-10-26 22:34:19 +0000

    Biters are now strong independent units

--- scripts/lualib/enemy-spawning.lua
+++ scripts/lualib/enemy-spawning.lua
@@ -1,48 +1,52 @@
--- uses global['unit-groups'] to track ... unit groups. {spawn, group}
+-- uses global['unit-tracking'] to track units unit_number = {spawn, entity}
 
+local function _goToSpawn(struct)
+	struct.entity.set_command{
+		type = defines.command.go_to_location,
+		destination = struct.spawn,
+		radius = 10
+	}
+end
+local function _wander(struct)
+	struct.entity.set_command{
+		type = defines.command.wander,
+		radius = 20
+	}
+end
 local function spawnGroup(surface,position,value)
 	-- TODO use "value" to determine size of threat
 	-- for now just spawn 4 biters at each point of interest
-	local group = surface.create_unit_group{position=position,force="enemy"}
+	if not global['unit-tracking'] then global['unit-tracking'] = {} end
 	for i=1,4 do
 		local pos = surface.find_non_colliding_position("big-biter", position, 10, 0.1)
 		if pos then
-			group.add_member(surface.create_entity{
+			local entity = surface.create_entity{
 				name = "big-biter",
 				position = pos,
 				force = game.forces.enemy
-			})
+			}
+			local struct = {
+				spawn = position,
+				entity = entity
+			}
+			global['unit-tracking'][entity.unit_number] = struct
+			_wander(struct)
 		end
-	end
-	if #group.members > 0 then
-		if not global['unit-groups'] then global['unit-groups'] = {} end
-		global['unit-groups'][group.group_number] = {
-			spawn = position,
-			group = group
-		}
 	end
 	-- TODO sometimes spawn gas cloud entities
 end
 local function onCommandCompleted(event)
-	local group = global['unit-groups'] and global['unit-groups'][event.unit_number]
-	if group then
-		-- by default, go to spawn point and 
-		group.group.set_command{
-			type = defines.command.compound,
-			structure_type = defines.compound_command.return_last,
-			commands = {
-				{
-					type = defines.command.go_to_location,
-					destination = group.spawn,
-					radius = 10
-				},
-				{
-					type = defines.command.wander,
-					radius = 20,
-					ticks_to_wait = 900
-				}
-			}
-		}
+	local struct = global['unit-tracking'] and global['unit-tracking'][event.unit_number]
+	if struct then
+		if struct.entity.valid then
+			if event.was_distracted then
+				_goToSpawn(struct)
+			else
+				_wander(struct)
+			end
+		else
+			global['unit-tracking'][event.unit_number] = nil
+		end
 	end
 end
 
