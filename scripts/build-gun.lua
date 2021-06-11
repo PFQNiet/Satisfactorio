7279e98bf8bdca9f834c3e1f6d5eff22f4a18013
Niet <niet@pokefarm.com>
2021-06-12 00:23:00 +0100

    Fix Railway placement in Build Gun

--- scripts/build-gun.lua
+++ scripts/build-gun.lua
@@ -1,3 +1,7 @@
+-- uses global.rail_freebies to give 5 free rails for every rail paid for
+-- this effectively reduces the cost of rails to 1/6
+local script_data = {} -- dictionary of player index to number of free rails granted
+
 local function canAfford(player, materials, inventory, buffer)
 	if player.cheat_mode then return 100 end
 	local contents = inventory.get_contents()
@@ -12,12 +16,17 @@ local function canAfford(player, materials, inventory, buffer)
 	end
 	return affordable
 end
-local function getUndoRecipe(prototype)
+local function getUndoRecipe(name)
+	if not name then return nil end
+	local test = game.recipe_prototypes[name.."-undo"]
+	if test then return test end
+	-- try looking up the entity
+	local prototype = game.entity_prototypes[name]
 	if not prototype then return nil end
 	local place = prototype.items_to_place_this
 	if not place then return nil end
 	for _,item in pairs(place) do
-		local test = game.recipe_prototypes[item.name.."-undo"]
+		test = game.recipe_prototypes[item.name.."-undo"]
 		if test then return test end
 	end
 end
@@ -25,7 +34,7 @@ end
 local function refundEntity(player, entity)
 	-- if the entity has an undo recipe, refund the components instead
 	if not (player and player.cheat_mode) then
-		local undo = getUndoRecipe(entity.prototype)
+		local undo = getUndoRecipe(entity.name)
 		local insert = undo and undo.products or {{name=entity.name,amount=1}}
 		for _,refund in pairs(insert) do
 			local spill = refund.amount
@@ -50,6 +59,7 @@ local function updateGUI(player, buffer)
 			type = "frame",
 			name = "buildgun",
 			direction = "vertical",
+			ignored_by_interaction = true,
 			style = "inner_frame_in_outer_frame"
 		}
 		gui.style.horizontally_stretchable = false
@@ -71,13 +81,14 @@ local function updateGUI(player, buffer)
 	gui.visible = false
 
 	local name = (player.cursor_stack.valid_for_read and player.cursor_stack.name) or (player.cursor_ghost and player.cursor_ghost.name) or ""
-	local undo = getUndoRecipe(game.entity_prototypes[name])
+	local undo = getUndoRecipe(name)
 	if not undo then return end
 	local inventory = player.get_main_inventory().get_contents()
 	if buffer then buffer = buffer.get_contents() else buffer = {} end
 	local cost = undo.products
 	
-	gui.caption = {"gui.build-gun-caption", name, game.item_prototypes[name].localised_name}
+	local item = game.item_prototypes[name]
+	gui.caption = {"gui.build-gun-caption", name, item.localised_name}
 	local list = gui.content.materials
 	list.clear()
 	local table = list.add{
@@ -89,11 +100,17 @@ local function updateGUI(player, buffer)
 	for i,product in pairs(cost) do
 		table.style.column_alignments[i] = "right"
 		-- first row: icons
+		local number = product.amount
+		if item.type == "rail-planner" then
+			if (script_data[player.index] or 0) > 0 then
+				number = 0.1*script_data[player.index]
+			end
+		end
 		local icon = table.add{
 			type = "sprite-button",
 			style = "transparent_slot",
 			sprite = "item/"..product.name,
-			number = product.amount,
+			number = number,
 			tooltip = game.item_prototypes[product.name].localised_name
 		}
 		icon.style.width = 64
@@ -117,7 +134,7 @@ end
 
 local function onCraft(event)
 	-- if the item to craft has an undo recipe, cancel the craft and put the item in the cursor
-	local undo = getUndoRecipe(game.entity_prototypes[event.recipe.prototype.main_product.name])
+	local undo = getUndoRecipe(event.recipe.prototype.main_product.name)
 	if undo then
 		local player = game.players[event.player_index]
 		-- find the craft in the queue - it should really be the only one under Satisfactorio rules but to be safe...
@@ -133,7 +150,8 @@ local function onCraft(event)
 		else
 			player.cancel_crafting{index=index,count=event.queued_count}
 			player.clear_cursor()
-			player.cursor_stack.set_stack{name=event.recipe.prototype.products[1].name, count=1}
+			local item = event.recipe.prototype.products[1].name
+			player.cursor_stack.set_stack{name=item, count=game.item_prototypes[item].stack_size}
 			if player.opened_self then player.opened = nil end
 		end
 	end
@@ -148,13 +166,13 @@ local function onCursorChange(event)
 	if player.cursor_stack.valid_for_read then return end
 	if not player.cursor_ghost then return end
 	local name = player.cursor_ghost.name
-	local undo = getUndoRecipe(game.entity_prototypes[name])
-	if not undo then return end
 	local redo = player.force.recipes[name]
 	if not (redo and redo.enabled) then return end
+	local undo = getUndoRecipe(name)
+	if not undo then return end
 	if canAfford(player, undo.products, player.get_main_inventory(), event.buffer) < 1 then return end
 	player.clear_cursor()
-	player.cursor_stack.set_stack{name=name, count=1}
+	player.cursor_stack.set_stack{name=name, count=game.item_prototypes[name].stack_size}
 	-- updateGUI(player)
 end
 
@@ -163,11 +181,14 @@ local function onBuilt(event)
 	local entity = event.created_entity
 	local name = entity.name
 	-- if the item in the cursor is an undo-able building, check if the player has enough stuff (something else may have pulled items from the player's inventory)
-	local undo = getUndoRecipe(game.entity_prototypes[name])
+	local undo = getUndoRecipe(name)
 	if not undo then return end
 
 	local inventory = player.get_main_inventory()
 	local afford = canAfford(player, undo.products, inventory)
+	if player.cursor_stack.valid_for_read and player.cursor_stack.type == "rail-planner" then
+		afford = afford*6 + (script_data[player.index] or 0)
+	end
 	local source = undo.ingredients[1].name
 	if afford < 1 then
 		-- this shouldn't happen, but may if another player or script changes the player's inventory (eg. drones...)
@@ -185,13 +206,24 @@ local function onBuilt(event)
 		return
 	end
 	if not player.cheat_mode then
-		-- items exist, now remove them
-		for _,product in pairs(undo.products) do
-			inventory.remove{name=product.name, count=product.amount}
+		local pay = true
+		-- special case: if the built item is from a rail planner, reduce cost by 1/6
+		if player.cursor_stack.valid_for_read and player.cursor_stack.type == "rail-planner" then
+			local cost = entity.type == "curved-rail" and 4 or 1
+			if (script_data[player.index] or 0) >= cost then pay = false end
+			afford = afford - cost
+			script_data[player.index] = afford % 6
+		end
+		if pay then
+			-- items exist, now remove them
+			for _,product in pairs(undo.products) do
+				inventory.remove{name=product.name, count=product.amount}
+			end
+			afford = afford-1
 		end
 	end
-	if afford > 1 then
-		player.cursor_stack.set_stack{name=source,count=1}
+	if afford > 0 then
+		player.cursor_stack.set_stack{name=source,count=math.min(afford,game.item_prototypes[source].stack_size)}
 	else
 		player.cursor_ghost = source
 	end
@@ -206,11 +238,25 @@ local function onRemoved(event)
 			-- if an undo recipe exists for this item, replace it with the undo results
 			local undo = game.recipe_prototypes[item.."-undo"]
 			if undo then
-				event.buffer.remove({name=item, count=count})
+				event.buffer.remove{name=item, count=count}
+				local proto = game.item_prototypes[item]
+				local pay = true
+				if proto.type == "rail-planner" then
+					-- don't refund rails that weren't paid for
+					local cost = event.entity.type == "curved-rail" and 4 or 1
+					script_data[player.index] = (script_data[player.index] or 0) + cost
+					pay = false
+					if script_data[player.index] >= 6 then
+						script_data[player.index] = script_data[player.index] % 6
+						pay = true
+					end
+				end
 				for _,product in pairs(undo.products) do
 					if not cheater then
-						-- undo recipes are always solids, and don't use probability stuff so fixed amounts
-						event.buffer.insert({name=product.name, count=product.amount})
+						if pay then
+							-- undo recipes are always solids, and don't use probability stuff so fixed amounts
+							event.buffer.insert{name=product.name, count=product.amount*count}
+						end
 					end
 				end
 			end
@@ -220,6 +266,15 @@ local function onRemoved(event)
 end
 
 return {
+	on_init = function()
+		global.rail_freebies = global.rail_freebies or script_data
+	end,
+	on_load = function()
+		script_data = global.rail_freebies or script_data
+	end,
+	on_configuration_changed = function()
+		if not global.rail_freebies then global.rail_freebies = script_data end
+	end,
 	events = {
 		[defines.events.on_pre_player_crafted_item] = onCraft,
 		[defines.events.on_player_cursor_stack_changed] = onCursorChange,
