6ee6585525d916b16d0071d33ca2492aabab54a1
mrchous <ivanchousKutovoy@gmail.com>
2022-01-31 20:01:55 +0300

    Fixed crash caused by poles tips&tricks
    The fix only addresses the crash, not the actual issue. Needs to be changed by someone who knows what they're doing.

---
+++ prototypes/tips-and-tricks/pole-dragging-coverage.lua
@@ -0,0 +1,11 @@
+return {
+	type = "tips-and-tricks-item",
+	name = "pole-dragging-coverage",
+	order = "c[pole-dragging-coverage]",
+	tag = "[item=hub-parts]",
+	trigger = {
+		type = "research",
+		technology = "hub-tier0-hub-upgrade4"
+	},
+	image = graphics.."tips-and-tricks/build-gun.png"
+}
